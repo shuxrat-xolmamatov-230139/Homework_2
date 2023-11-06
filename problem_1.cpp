509fd00aad37ca1c6dcd0f03a2f6c87c934ba5fa
shuxrat-xolmamatov-230139 <148914589+shuxrat-xolmamatov-230139@users.noreply.github.com>
2023-11-06 13:50:52 +0500

    Update problem_1.cpp

--- problem_1.cpp
+++ problem_1.cpp
@@ -9,7 +9,7 @@ int main () {
     float average = float(a + b + c)/3;
     int product = a * b * c;
 
-    cout << "The result of average is equal to " << average << endl << "The result of product is equal to " << product << endl;
+   
 
   return 0;
 }
