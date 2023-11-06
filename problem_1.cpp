cfc2eae08869e4b7b3e26298750cacd84d3c1fd9
shuxrat-xolmamatov-230139 <148914589+shuxrat-xolmamatov-230139@users.noreply.github.com>
2023-11-06 16:13:21 +0500

    Update problem_1.cpp

--- problem_1.cpp
+++ problem_1.cpp
@@ -1,11 +1,8 @@
-
-
-void mainAns(float num1, float num2, float arr[]){
+void problemSolution(float num1, float num2, float arr[]){
     float average, product;
     average = (num1 + num2) / 2;
-    product = num1*num2 ;
+    product = num1 * num2 ;
 
     arr[0] = average;
     arr[1] = product;
-
 }
