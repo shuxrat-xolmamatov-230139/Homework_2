aa47459e5026d54519907227c224541556f5a2da
shuxrat-xolmamatov-230139 <148914589+shuxrat-xolmamatov-230139@users.noreply.github.com>
2023-11-06 14:40:38 +0500

    Update problem_1.cpp

--- problem_1.cpp
+++ problem_1.cpp
@@ -2,14 +2,11 @@
 
 using namespace std;
 
-int main () {
+void problemSolution(float num1, float num2, float arr[]){
+    float average, product;
+    average = (num1 + num2) / 2;
+    product = num1*num2 ;
 
-    int a, b, c;
-    cin >> a >> b >> c;
-    float average = float(a + b + c)/3;
-    int product = a * b * c;
-
-    cout << "The result of average is equal to " << average << endl << "The result of product is equal to " << product << endl;
-
-  return 0;
+    arr[0] = average;
+    arr[1] = product;
 }
