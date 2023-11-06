56ebff876a4f38575dd04aa0b5b3d146523114bc
shuxrat-xolmamatov-230139 <148914589+shuxrat-xolmamatov-230139@users.noreply.github.com>
2023-11-06 16:03:05 +0500

    Update problem_2.cpp

--- problem_2.cpp
+++ problem_2.cpp
@@ -2,12 +2,19 @@
 
 using namespace std;
 
-int main(){
+// int main(){
 
-    int base, height;
-    cin >> base >> height;
+//     int base, height;
+//     cin >> base >> height;
 
-    cout << "Area of triangle is equal to " << float(base * height) / 2;
+//     cout << "Area of triangle is equal to " << float(base * height) / 2;
 
-    return 0;
+//     return 0;
+// }
+
+void mainAns(int base, int height, float arr[]){
+    float area;
+    area = (base * height) / 2;
+
+    arr[0] = area;
 }
